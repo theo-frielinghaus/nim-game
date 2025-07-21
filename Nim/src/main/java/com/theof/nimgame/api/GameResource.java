aca31daa2f33b90436d053b1cf372bb397cbe09d
Theo Frielinghaus <theo.frielinghaus@bsb.hamburg.de>
2025-07-21 03:06:21 +0200

    style: add api documentation

--- Nim/src/main/java/com/theof/nimgame/api/GameResource.java
+++ Nim/src/main/java/com/theof/nimgame/api/GameResource.java
@@ -8,6 +8,7 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 
 @Path("/games")
@@ -16,6 +17,16 @@ public class GameResource {
     GameService gameService;
 
     @POST
+    @Operation(
+        summary = "Create and start a new game",
+        description = """
+    Creates a game using the provided settings. \n
+    Current only functioning setting is {"comStrategy": "random"}.\n
+    Returns the game state at the start of the game, with the gameID that is needed to make moves.\n
+    If computer starts the game, they already start with their own move, which will be reflected in game state.
+    
+    """
+    )
     public GameStateDTO startGame(SettingsDTO settings) {
         Long gameId = gameService.createGame(settings);
         return gameService.startGame(gameId, true);
@@ -23,6 +34,17 @@ public class GameResource {
 
     @PUT
     @Path("{gameID}")
+    @Operation(
+        summary = "Make a Move",
+        description = """
+    Makes a move with provided number of sticks to take for a running game. \n
+    Legal move is an amount of sticks to take between 1-3 that is not bigger than current stick count. \n
+    The computer reacts immediately with their own move, included in returned game state. \n
+    If a move ends a game, the winner will be included in game state.\n
+    The log included in gamestate gives additional information about computer player moves.\n
+    
+    """
+    )
     public GameStateDTO makeMove(@PathParam("gameID") Long gameID, MoveDTO move) {
         return gameService.makeMove(gameID, move);
     }
