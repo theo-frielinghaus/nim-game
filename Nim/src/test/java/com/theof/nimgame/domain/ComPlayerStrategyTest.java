5e0b3a06f2aae54e273c38fc8784a80a9d00f9f0
Theo Frielinghaus <theo.frielinghaus@bsb.hamburg.de>
2025-07-21 19:11:29 +0200

    test: add tests for optimal strategy

--- Nim/src/test/java/com/theof/nimgame/domain/ComPlayerStrategyTest.java
+++ Nim/src/test/java/com/theof/nimgame/domain/ComPlayerStrategyTest.java
@@ -4,6 +4,8 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
@@ -66,13 +68,62 @@ class ComPlayerStrategyTest {
 
     @Nested
     class OptimalMoveStrategyTest {
+        final String STRATEGY_NAME = "optimal";
 
         @Test
         void optimal_move_strategy_creation() {
-            final var STRATEGY_NAME = "OPTIMAL";
 
             ComPlayerStrategy testee = ComPlayerStrategy.fromType(STRATEGY_NAME);
             assertThat(testee).isExactlyInstanceOf(OptimalMoveStrategy.class);
+        }
+
+
+        @ParameterizedTest
+        @ValueSource(ints = {10,11,12})
+        void optimal_early_game_move_is_leaving_9_on_pile(int stickCount) {
+            var pileBeforeMove = new Pile(stickCount);
+            ComPlayerStrategy testee = ComPlayerStrategy.fromType(STRATEGY_NAME);
+
+            var move = testee.makeMove(pileBeforeMove);
+
+            assertThat(move).isNotNull();
+            assertThat(move.sticksToTake() + 9).isEqualTo(stickCount);
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {6,7,8})
+        void optimal_mid_game_move_is_leaving_5_on_pile(int stickCount) {
+            var pileBeforeMove = new Pile(stickCount);
+            ComPlayerStrategy testee = ComPlayerStrategy.fromType(STRATEGY_NAME);
+
+            var move = testee.makeMove(pileBeforeMove);
+
+            assertThat(move).isNotNull();
+            assertThat(move.sticksToTake() + 5).isEqualTo(stickCount);
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {2,3,4})
+        void optimal_mid_game_move_is_leaving_1_on_pile(int stickCount) {
+            var pileBeforeMove = new Pile(stickCount);
+            ComPlayerStrategy testee = ComPlayerStrategy.fromType(STRATEGY_NAME);
+
+            var move = testee.makeMove(pileBeforeMove);
+
+            assertThat(move).isNotNull();
+            assertThat(move.sticksToTake() + 1).isEqualTo(stickCount);
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {5,9,13})
+        void optimal_move_from_losing_position_is_take_1_from_pile(int stickCount){
+            var pileBeforeMove = new Pile(stickCount);
+            ComPlayerStrategy testee = ComPlayerStrategy.fromType(STRATEGY_NAME);
+
+            var move = testee.makeMove(pileBeforeMove);
+
+            assertThat(move).isNotNull();
+            assertThat(move.sticksToTake()).isEqualTo(1);
         }
     }
 }
