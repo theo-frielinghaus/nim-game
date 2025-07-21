b784783987f5b1fdca1c014984cd8fba94f916a1
Theo Frielinghaus <theo.frielinghaus@bsb.hamburg.de>
2025-07-21 03:20:23 +0200

    refactor: remove unused bootstrapped quarkus test

--- Nim/src/test/java/com/theof/nimgame/api/GameResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.theof.nimgame.api;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GameResourceIT extends GameResourceTest {
-    // Execute the same tests but in packaged mode. Ensures true black box testing of the application
-}
